43ed659f2c52ad43039f89141c5ce04aaa6c5235
agandar1 <agandarilla05@protonmail.com>
2021-02-10 06:45:13 -0600

    Better map borders

--- game/main.py
+++ game/main.py
@@ -6,6 +6,7 @@ from pygame.locals import *
 import levels.lvl1
 
 # Colors
+BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 RED = (255, 0, 0)
@@ -20,18 +21,21 @@ pygame.display.set_caption("World's Hardest Game")
 class Game:
     "Class to draw the map"
     # setup levels
-    # lvln = tile_list
-    lvl1 = [levels.lvl1.tile_list]
+    # lvln = tile_list, border_list
+    lvl1 = [levels.lvl1.tile_list, levels.lvl1.border_list]
     lvls = [lvl1]
 
     def __init__(self, starting):
         self.level = starting - 1
         self.tile_list = self.lvls[self.level][0]
+        self.border_list = self.lvls[self.level][1]
 
     def draw(self):
         "Draw all the tiles"
         for tile in self.tile_list:
             WIN.blit(tile[0], tile[1])
+        for line in self.border_list:
+            pygame.draw.line(WIN, BLACK, line[0], line[1], 4)
         pygame.display.update()
 
 
